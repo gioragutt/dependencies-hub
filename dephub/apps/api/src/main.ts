9b82cf67ff4bc5ec5fb54c423f35deffa01c7e45
Giora Guttsait <giora111@gmail.com>
2020-08-08 17:06:47 +0300

    Play around with neo4j

--- dephub/apps/api/src/main.ts
+++ dephub/apps/api/src/main.ts
@@ -4,11 +4,25 @@
  */
 
 import * as express from 'express';
+import * as neo4j from 'neo4j-driver';
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+const driver = neo4j.driver('bolt://localhost:db');
+
+const session = driver.session();
+
+app.get('/api', async (req, res) => {
+  res.json(await session.run('MATCH (n) RETURN n'));
+});
+
+app.get('/modules', async (req, res) => {
+  const query = `
+    MATCH (lib:Module)<-[vl:VERSION_OF]-(lv:ModuleVersion)<-[d:DEPENDS_ON_LIBRARY]-(sv)-[:VERSION_OF]->(s)
+    RETURN s.name as Service, lib.name as Library, lv.version as LibraryVersion, sv.version as ServiceVersion
+  `;
+
+  const response = await session.run(query);
 });
 
 const port = process.env.port || 3333;
